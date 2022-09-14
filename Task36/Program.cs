b71488f7550c490edc35e4d1415492eb2790013a
burmasovanton <looseangel1@gmail.com>
2022-09-14 19:10:23 +0500

    new addition

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -21,12 +21,11 @@ int[] GetArray(int size, int leftRange, int RightRange)
 int FindSummOfOddNumbers(int[] arr)
 {
 int sum = 0;
-   for (int i = 0; i < arr.Length; i++)
+   for (int i = 1; i < arr.Length; i = i + 2)
    {
-    if(i % 2 != 0)
-    {
-      sum = sum + arr[i];
-    }
+    
+    sum = sum + arr[i];
+    
    }
     
 return sum;
